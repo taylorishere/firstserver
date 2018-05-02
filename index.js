98dc62a28228814163ca2724d248711ac1825b5f
taylor <taylorishere@gmail.com>
2018-05-02 11:10:29 -0700

    does this work yet?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
