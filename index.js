877ecc32428dadf157f83fabbef3abab2ae66976
taylor <taylorishere@gmail.com>
2018-05-03 09:03:00 -0700

    added Heroku port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log('--SERVER STARTED--'));
+app.listen(port, () => console.log('--SERVER STARTED--'));
